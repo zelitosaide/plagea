ee3a90fa9ee86e98449744111c19010a1dbabc81
Zelito A. Saide <zelitosaide83@gmail.com>
2025-06-02 21:47:13 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Ola Tudo bem?
       <div>
-        Estou bem e você??
+        Estou bem e você???
       </div>
     </div>
   );
