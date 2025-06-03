8ba9d97a6fbb1bc96074be01e6cd69758e59e202
Zelito A. Saide <zelitosaide83@gmail.com>
2025-06-03 22:16:24 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <button className="bg-red-200">Click aqui!</button>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Next.js with Tailwind CSS!
+      </h1>
+      <p className="mt-4 text-lg">
+        This is a simple example of a Next.js page styled with Tailwind CSS.
+      </p>
+    </>
   );
 }
