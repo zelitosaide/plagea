35538948cfe6e970445bbb5c1b8b41c33d8025b0
Zelito Atumane Saide <zelitosaide@Zelitos-MacBook-Pro.local>
2025-11-04 18:49:47 +0200

    ..

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,8 +1,8 @@
 import { MongoClient, type Db } from "mongodb";
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your MongoDB URI to .env.local");
-}
+// if (!process.env.MONGODB_URI) {
+//   throw new Error("Please add your MongoDB URI to .env.local");
+// }
 
 // const uri = process.env.MONGODB_URI
 const uri = process.env.MONGODB_URI
